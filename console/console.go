c20dadbd1d6fb23e61db138bd4d12f8d04c50076
str1ngs <mike.rosset@gmail.com>
2012-03-09 17:56:23 -0800

    use enumberator for byte size formatting.

--- console/console.go
+++ console/console.go
@@ -28,8 +28,7 @@ func (self *ProgressBarWriter) Write(b []byte) (n int, err error) {
 		progress := (width * percent) / 100
 		bar := strings.Repeat("#", int(progress))
 		bps := float64(self.done) / time.Now().Sub(self.start).Seconds()
-		//fmt.Printf("\r%-20.20s [%-*s] %3.3s%%", self.prefix, width, bar, strconv.Itoa(percent))
-		fmt.Printf("\r%-20.20s [%-*s] %s %3.3s%%", self.prefix, width, bar, speed(bps), strconv.Itoa(percent))
+		fmt.Printf("\r%-20.20s [%-*s] %s %3.3s%%", self.prefix, width, bar, ByteSize(bps), strconv.Itoa(percent))
 	default:
 		fmt.Printf("\r%-20.20s", self.prefix)
 	}
@@ -40,16 +39,38 @@ func NewProgressBarWriter(p string, t int64, w io.Writer) *ProgressBarWriter {
 	return &ProgressBarWriter{prefix: p, total: t, w: w}
 }
 
-func speed(b float64) string {
+type ByteSize float64
+
+const (
+	_           = iota // ignore first value by assigning to blank identifier
+	KB ByteSize = 1 << (10 * iota)
+	MB
+	GB
+	TB
+	PB
+	EB
+	ZB
+	YB
+)
+
+func (b ByteSize) String() string {
 	switch {
-	case b < 1024:
-		return fmt.Sprintf("%vB/s", b)
-	case b < 1024*1000:
-		return fmt.Sprintf("%5.1fKB/s", b/1024)
-	case b < 1024*1024*1000:
-		return fmt.Sprintf("%5.1fMB/s", b/1024/1024)
-	default:
-		return fmt.Sprintf("%5.1fGB/s", b/1024/1024/1024)
+	case b >= YB:
+		return fmt.Sprintf("%5.1fYB", float64(b/YB))
+	case b >= ZB:
+		return fmt.Sprintf("%5.1fZB", float64(b/ZB))
+	case b >= EB:
+		return fmt.Sprintf("%5.1fEB", float64(b/EB))
+	case b >= PB:
+		return fmt.Sprintf("%5.1fPB", float64(b/PB))
+	case b >= TB:
+		return fmt.Sprintf("%5.1fTB", float64(b/TB))
+	case b >= GB:
+		return fmt.Sprintf("%5.1fGB", float64(b/GB))
+	case b >= MB:
+		return fmt.Sprintf("%5.1fMB", float64(b/MB))
+	case b >= KB:
+		return fmt.Sprintf("%5.1fKB", float64(b/KB))
 	}
-	return fmt.Sprintf("%5.1fGB/s", b/1024/1024/1024)
+	return fmt.Sprintf("%.2fB", float64(b))
 }
