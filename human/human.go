b0f58852ccf8738ce661d962cc347eda9105a073
mike rosset <mike.rosset@gmail.com>
2012-08-18 13:03:52 +0000

    use %6 field width to handle 1000.0

--- human/human.go
+++ human/human.go
@@ -21,21 +21,21 @@ const (
 func (b ByteSize) String() string {
 	switch {
 	case b >= YB:
-		return fmt.Sprintf("%5.1fYB", float64(b/YB))
+		return fmt.Sprintf("%6.1fYB", float64(b/YB))
 	case b >= ZB:
-		return fmt.Sprintf("%5.1fZB", float64(b/ZB))
+		return fmt.Sprintf("%6.1fZB", float64(b/ZB))
 	case b >= EB:
-		return fmt.Sprintf("%5.1fEB", float64(b/EB))
+		return fmt.Sprintf("%6.1fEB", float64(b/EB))
 	case b >= PB:
-		return fmt.Sprintf("%5.1fPB", float64(b/PB))
+		return fmt.Sprintf("%6.1fPB", float64(b/PB))
 	case b >= TB:
-		return fmt.Sprintf("%5.1fTB", float64(b/TB))
+		return fmt.Sprintf("%6.1fTB", float64(b/TB))
 	case b >= GB:
-		return fmt.Sprintf("%5.1fGB", float64(b/GB))
+		return fmt.Sprintf("%6.1fGB", float64(b/GB))
 	case b >= MB:
-		return fmt.Sprintf("%5.1fMB", float64(b/MB))
+		return fmt.Sprintf("%6.1fMB", float64(b/MB))
 	case b >= KB:
-		return fmt.Sprintf("%5.1fKB", float64(b/KB))
+		return fmt.Sprintf("%6.1fKB", float64(b/KB))
 	}
-	return fmt.Sprintf("%5.0f B", float64(b))
+	return fmt.Sprintf("%6.0f B", float64(b))
 }
