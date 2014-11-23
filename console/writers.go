9ce62da4f10d818eda049c27b51e8a8abe0f58a8
mike rosset <mike.rosset@gmail.com>
2014-11-22 22:42:22 -0800

    write prefix at end of ProgressBarWriter

--- console/writers.go
+++ console/writers.go
@@ -17,26 +17,26 @@ type ProgressBarWriter struct {
 	prefix string
 }
 
-func (self *ProgressBarWriter) Write(b []byte) (n int, err error) {
-	if self.done == 0 {
-		self.start = time.Now()
+func (pbw *ProgressBarWriter) Write(b []byte) (n int, err error) {
+	if pbw.done == 0 {
+		pbw.start = time.Now()
 	}
 	switch {
-	case self.total > 0:
-		self.done += int64(len(b))
-		percent := int((self.done * 100) / self.total)
+	case pbw.total > 0:
+		pbw.done += int64(len(b))
+		percent := int((pbw.done * 100) / pbw.total)
 		width := (80 - 9) - 32
 		progress := (width * percent) / 100
 		bar := strings.Repeat("#", int(progress))
-		bps := float64(self.done) / time.Now().Sub(self.start).Seconds()
-		fmt.Printf("\r%-20.20s [%-*s] %s/s %3.3s%%", self.prefix, width, bar, human.ByteSize(bps), strconv.Itoa(percent))
+		bps := float64(pbw.done) / time.Now().Sub(pbw.start).Seconds()
+		fmt.Printf("\r[%-*s] %s/s %3.3s%% %-20.20s", width, bar, human.ByteSize(bps), strconv.Itoa(percent), pbw.prefix)
 	default:
-		fmt.Printf("\r%-20.20s", self.prefix)
+		fmt.Printf("\r%-20.20s", pbw.prefix)
 	}
-	return self.w.Write(b)
+	return pbw.w.Write(b)
 }
 
-func (self *ProgressBarWriter) Close() error {
+func (pbw *ProgressBarWriter) Close() error {
 	fmt.Println()
 	return nil
 }
